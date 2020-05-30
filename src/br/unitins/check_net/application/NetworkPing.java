6a8e721f613d92befb0bad349f2fcbc7a199d3b5
ThalysonLira <thalysonliratl.ln@gmail.com>
2020-05-30 02:08:35 -0300

    correções em listagem de serviços

--- src/br/unitins/check_net/application/NetworkPing.java
+++ src/br/unitins/check_net/application/NetworkPing.java
@@ -1,7 +1,51 @@
 package br.unitins.check_net.application;
 
-public abstract class NetworkPing {
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
 
-	// criar verificações de conexão
+public abstract class NetworkPing {
 	
+	public static String getHostName() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+			
+		} catch (UnknownHostException e) {
+			return "\tErro ao processar solicitação: " + e.toString();
+		}
+	}
+
+	public static String getIpInternal() {
+		try {
+			return InetAddress.getLocalHost().getHostAddress();
+			
+		} catch (UnknownHostException e) {
+			return "\tErro ao processar solicitação: " + e.toString();
+		}
+	}
+	
+	public static String getIpExternal() {
+		String ip = null;
+
+		try {
+			URL url = new URL("http://checkip.dyndns.org/");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			
+			connection.connect();
+			
+			java.io.BufferedReader page = new java.io.BufferedReader(
+					new java.io.InputStreamReader(connection.getInputStream()));
+			
+			String myIP = page.readLine();
+			ip = myIP.substring(myIP.indexOf(": ") + 2, myIP.lastIndexOf("</body>"));
+			page.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ip;
+	}
+	
+	// criar verificações de conexão
 }
--- src/br/unitins/check_net/application/Util.java
+++ src/br/unitins/check_net/application/Util.java
@@ -1,40 +1,21 @@
 package br.unitins.check_net.application;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class Util {
 	
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) {
 		
-		
-		Runtime runtime = Runtime.getRuntime();
-
-		String cmds[] = {"cmd", "/c", "tasklist"};
-
-		Process proc = runtime.exec(cmds);
-
-		InputStream inputstream = proc.getInputStream();
-
-		InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-
-		BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-
-		String line;
-
-		while ((line = bufferedreader.readLine()) != null) {
-
-			System.out.println(line);
-
-		}
-		
+		// Componentes operacionais
 		System.out.println("\nSistema operacional: " + OperationalComponents.getOperationalSystem());
 		System.out.println("Espaço disponível em disco (C:): " + OperationalComponents.getPathFreeSize("C:") + " bytes");
 		System.out.println("Porcentagem do disco disponível (D:): " + OperationalComponents.getPathFreePercent("C:") + " %");
 		System.out.println("Quantidade de memória RAM utilizada: "+ OperationalComponents.getMemoryUsedSize()  + " bytes");
 		System.out.println("Porcentagem de memória RAM livre: " + OperationalComponents.getMemoryFreePercent() + " %");
+		System.out.println("\nProcessos em execução: " + OperationalComponents.getRunningProcess("/C"));
+		
+		// Componentes de rede
+		System.out.println("IP interno: " + NetworkPing.getIpInternal());
+		System.out.println("Host: " + NetworkPing.getHostName());
+		System.out.println("IP externo: " + NetworkPing.getIpExternal());
 		
 	}
 	
